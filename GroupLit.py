a8240d8f95c73d917046c8a6d2adfb4b179ab5b0
ltdyer <48657041+ltdyer@users.noreply.github.com>
2019-03-23 23:20:00 -0400

    Python program using serial module
    Continuously reads in input, trying to get it to work with the button input

---
+++ GroupLit.py
@@ -0,0 +1,23 @@
+import serial
+import requests
+
+#use requests to send data to a url (want it to be a post)
+
+connected = False
+ser = serial.Serial(
+    port = 'tty96B0', #/dev/ttyUSB0 on Linux
+    baudrate = 9600, #9600 on the DragonBoard
+    timeout = 3
+)
+ser.open()
+if ser.isOpen():
+    print(ser.name + " is Open!")
+    connected = True
+
+
+while connected == True:
+    print('Test')
+    if ser.inWaiting() >= 0:
+        reading = ser.read(ser.inWaiting()).decode('ascii')
+        print(reading, end='')
+    #other code
