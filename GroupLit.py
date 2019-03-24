8c8d5091f54523652df0b7572a8f85786d2b3cf4
ltdyer <48657041+ltdyer@users.noreply.github.com>
2019-03-24 02:27:55 -0400

    took out params from readline
    yeah

--- GroupLit.py
+++ GroupLit.py
@@ -5,21 +5,24 @@ import requests
 
 connected = False
 ser = serial.Serial(
-    port = '/dev/tty96B0', #/dev/ttyUSB0 on Linux
+    port = 'dev/tty96B0', #/dev/ttyUSB0 on Linux
     baudrate = 9600, #9600 on the DragonBoard
     timeout = 3
 )
-if not ser.isOpen():
-    ser.open()
+ser.open()
 if ser.isOpen():
     print(ser.name + " is Open!")
     connected = True
 
+#we want to send the request when the button is pushed but how do we know when the button is pushed
+#we cant have the button press in arduino activate the request because its the wrong program
 
 while connected == True:
-    #print('Test')
-    if ser.inWaiting() >= 0:
-        print("we actually get some input")
-        reading = ser.read(ser.inWaiting()).decode('ascii')
+    print('Test')
+    if ser.inWaiting() > 0:
+        reading = ser.readline().decode('ascii')
         print(reading, end='')
-    #other code
+    data = {'key1' : 'value1', 'key2' : 'value2'}
+    request = requests.post('https://litme.net/api/dinner', data=data)
+    status = request.status_code
+    print("status is: ", status)
